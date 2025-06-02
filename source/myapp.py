604bf77ac7bcedbf363d622932c900dd659f8030
Michael Lukachev <michael.lukachev@test.com>
2025-06-02 20:36:45 +0200

    init commit

---
+++ source/myapp.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/healthz")
+def health():
+    return jsonify({"status": "UP"}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
